5da6df5a44161d284533e2d90265232f89f4e869
Marcelo Mouta <fc51110@alunos.fc.ul.pt>
2020-02-23 13:04:34 +0000

    Revert "renamed Mequie Class to MequieClient and extracted ServerThread Class to new filce"
    This reverts commit 03b45013

--- src/MequieServer.java
+++ src/MequieServer.java
@@ -5,6 +5,8 @@
 ***************************************************************************/
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -14,6 +16,7 @@ public class MequieServer{
 
 	public static void main(String[] args) {
 		System.out.println("servidor: main");
+		MequieServer server = new MequieServer();
 
 		if (args.length != 1) {
 			System.out.println("Numero de argumentos invalido. Exemplo de uso:\n\tMequieServer <port>");
@@ -21,17 +24,14 @@ public class MequieServer{
 		}
 
 		try {
-			int port = Integer.parseInt(args[0]);
-			startServer(port);
-
+			server.startServer(Integer.parseInt(args[0]));
 		} catch (NumberFormatException e) {
 			System.out.println("O porto tem que ser um numero");
-			System.exit(-1);
 		}
-
 	}
 
-	private static void startServer(int port){
+	private void startServer(int port){
+//		ServerSocket sSoc = null;
         
 		try (ServerSocket sSoc = new ServerSocket(port)){
 
@@ -56,4 +56,62 @@ public class MequieServer{
 		// sSoc.close();
 	}
 
+
+	//Threads utilizadas para comunicacao com os clientes
+	class ServerThread extends Thread {
+
+		private Socket socket = null;
+
+		ServerThread(Socket inSoc) {
+			socket = inSoc;
+			System.out.println("thread do server para cada cliente");
+		}
+ 
+		public void run(){
+			try {
+				ObjectOutputStream outStream = new ObjectOutputStream(socket.getOutputStream());
+				ObjectInputStream inStream = new ObjectInputStream(socket.getInputStream());
+
+				String user = null;
+				String passwd = null;
+			
+				try {
+					user = (String)inStream.readObject();
+					passwd = (String)inStream.readObject();
+					System.out.println("thread: depois de receber a password e o user");
+				}catch (ClassNotFoundException e1) {
+					e1.printStackTrace();
+				}
+
+
+				if (user.equals("user01") && passwd.equals("passwd")){
+					outStream.writeObject(true);
+
+					System.out.println("user01 foi autenticado com sucesso.");
+					System.out.println("A receber ficheiro...");
+
+					int fileSize = (Integer) inStream.readObject();
+
+					byte[] fileBuf = new byte[fileSize];
+					inStream.read(fileBuf,0,fileSize);
+
+					System.out.println("Ficheiro recebido.");
+				}
+				else {
+					outStream.writeObject(false);
+					System.out.println("Autenticacao falhou: username ou password incorretos");
+				}
+
+				outStream.close();
+				inStream.close();
+ 			
+				socket.close();
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
