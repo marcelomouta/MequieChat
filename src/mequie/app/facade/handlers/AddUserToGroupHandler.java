ae5d820ebfc37d1d9fb51713cc22c3112cb49e4f
BFreitas16 <fc51468@alunos.fc.ul.pt>
2020-03-04 12:52:02 +0000

    AddUserToGroupHandler organizado e metodo save que escreve em disco feito

--- src/mequie/app/facade/handlers/AddUserToGroupHandler.java
+++ src/mequie/app/facade/handlers/AddUserToGroupHandler.java
@@ -1,33 +1,54 @@
 package mequie.app.facade.handlers;
 
+import java.io.IOException;
+
 import mequie.app.domain.Group;
 import mequie.app.domain.User;
 import mequie.app.domain.catalogs.GroupCatalog;
 import mequie.app.domain.catalogs.UserCatalog;
 import mequie.app.facade.Session;
 import mequie.app.facade.exceptions.ErrorAddingUserToGroupException;
+import mequie.app.facade.exceptions.ErrorSavingGroupInDiskException;
 import mequie.app.facade.exceptions.NotExistingGroupException;
 import mequie.app.facade.exceptions.NotExistingUserException;
+import mequie.utils.WriteInDisk;
 
 public class AddUserToGroupHandler{
 
     private User currentUser;
+    
+    private Group currentGroup;
+    
+    private User currentUserToAdd;
 
     public AddUserToGroupHandler(Session s) {
         currentUser = s.getUser();
     }
 
-    public void indicateUserIDAndGroupID(String userID, String groupID) throws ErrorAddingUserToGroupException, NotExistingGroupException, NotExistingUserException {
-        User userToAdd = UserCatalog.getInstance().getUserById(userID);
-        if (userToAdd == null)
+    public void getUserByIDAndGroupID(String userID, String groupID) throws NotExistingUserException {
+    	currentUserToAdd = UserCatalog.getInstance().getUserById(userID);
+        if (currentUserToAdd == null)
             throw new NotExistingUserException();
-
-        Group currentGroup = GroupCatalog.getInstance().getGroupByID(groupID);
-        if  (currentGroup == null) 
+    }
+    
+    public void getGroupByID(String groupID) throws NotExistingGroupException {
+    	currentGroup = GroupCatalog.getInstance().getGroupByID(groupID);
+    	if  (currentGroup == null) 
             throw new NotExistingGroupException();
-        
-        if ( !currentGroup.addUserByID(userToAdd) )
+    }
+    
+    public void addNewUserToGroup() throws ErrorAddingUserToGroupException {
+    	if ( !currentGroup.addUserByID(currentUserToAdd) )
             throw new ErrorAddingUserToGroupException();
+    }
+    
+    public void save() throws ErrorSavingGroupInDiskException {
+    	try {
+			WriteInDisk write = new WriteInDisk("Data/group");
+			write.saveStringSeparatedBy(currentUserToAdd.getUserID(), ":");
+		} catch (IOException e) {
+			throw new ErrorSavingGroupInDiskException();
+		}
     }
 
 }
