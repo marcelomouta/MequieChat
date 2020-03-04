3b3f69e26a661abd2ec78ba310672a6127ef1e82
BFreitas16 <fc51468@alunos.fc.ul.pt>
2020-03-04 12:24:37 +0000

    added methods to write a string and a List of Strings

--- src/mequie/utils/WriteInDisk.java
+++ src/mequie/utils/WriteInDisk.java
@@ -1,5 +1,37 @@
 package mequie.utils;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
 public class WriteInDisk {
 
+	private FileWriter writer;
+	
+	public WriteInDisk(String fileLocation) throws IOException {
+		writer = new FileWriter(fileLocation, true);
+	}
+	
+	public boolean saveStringSeparatedBy(String toSave) {
+		try {
+			writer.write(toSave);
+			return true;
+		} catch (IOException e) {
+			return false;
+		}
+	}
+	
+	public boolean saveListOfStringsSeparatedBy(List<String> itemsToSave, String sep) {
+		try {
+			for (String item : itemsToSave) {
+				writer.write(sep);
+				writer.write(item);
+			}
+			return true;
+		} catch (IOException e) {
+			return false;
+		}
+	}
+	
+	
 }
