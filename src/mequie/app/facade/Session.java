ecdfda61586c8d15cd8fb48d6b708548a5aabdcc
Pedro Marques <fc51021@alunos.fc.ul.pt>
2020-03-04 22:00:54 +0000

    Adicionado a Session o login de users existentes

--- src/mequie/app/facade/Session.java
+++ src/mequie/app/facade/Session.java
@@ -3,13 +3,20 @@ package mequie.app.facade;
 import java.io.Serializable;
 
 import mequie.app.domain.User;
+import mequie.app.domain.catalogs.UserCatalog;
 
 public class Session implements Serializable {
 
     private User currentUser;
+    private UserCatalog userCat = UserCatalog.getInstance();
 
     public Session(String username, String password) {
-        this.currentUser = new User(username, password);
+    	this.currentUser = this.userCat.getUserById(username);
+    	// se o user ainda nao foi criado, cria-se esse user
+    	if(this.currentUser == null) {
+            this.currentUser = this.userCat.createUser(username, password);
+            this.userCat.addUser(this.currentUser);
+    	}
     }
 
     public User getUser() {
