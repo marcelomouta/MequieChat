00b499e1b623e0144afca5d985e7642071582b01
Pedro Marques <fc51021@alunos.fc.ul.pt>
2020-03-05 21:18:47 +0000

    Utilizador consegue iniciar sessão de novo

--- src/mequie/app/facade/Session.java
+++ src/mequie/app/facade/Session.java
@@ -8,14 +8,14 @@ import mequie.app.domain.catalogs.UserCatalog;
 public class Session implements Serializable {
 
     private User currentUser;
-    private UserCatalog userCat = UserCatalog.getInstance();
 
     public Session(String username, String password) {
-    	this.currentUser = this.userCat.getUserById(username);
+    	UserCatalog userCat = UserCatalog.getInstance();
+    	this.currentUser = userCat.getUserById(username);
     	// se o user ainda nao foi criado, cria-se esse user
     	if(this.currentUser == null) {
-            this.currentUser = this.userCat.createUser(username, password);
-            this.userCat.addUser(this.currentUser);
+            this.currentUser = userCat.createUser(username, password);
+            userCat.addUser(this.currentUser);
     	}
     }
 
