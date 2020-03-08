aee924b8121de2e89d17529b490016e3f4eaa6c9
pedro <pedro@10.101.220.107>
2020-03-08 13:52:21 +0000

    Criado NetworkMessageError para enviar erros do servidor

--- src/NetworkMessageResponse.java
+++ src/NetworkMessageResponse.java
@@ -7,24 +7,18 @@ import mequie.app.facade.exceptions.MequieException;
 public class NetworkMessageResponse extends NetworkMessage {
 	
 	private String result;
-	private MequieException exception;
 	
-	public NetworkMessageResponse(Opcode op, String result, MequieException exception) {
+	public NetworkMessageResponse(Opcode op, String result) {
 		super(op);
 		this.result = result;
-		this.exception = exception;
 	}
 	public String getResult() {
 		return result;
-	}
-	public MequieException getException() {
-		return exception;
 	}
 	
 	public String toString() {
 		return "--- NetworkMessageResponse ---\n" +
 			"Opcode = " + this.getOp().toString() + "\n" +
-			"Result = " + (this.getResult().equals("") ? "\"\"" : this.getResult()) + "\n" +
-			"Exception = " + this.getException().getMessage();
+			"Result = " + (this.getResult().equals("") ? "\"\"" : this.getResult());
 	}
 }
