9f024a21fbd314dba8692885bb80ff7a672fbe67
Bruno Freitas <fc51468@alunos.fc.ul.pt>
2020-03-15 18:56:55 +0000

    created const with the data path

--- server/src/mequie/app/facade/handlers/SaveToDiskHandler.java
+++ server/src/mequie/app/facade/handlers/SaveToDiskHandler.java
@@ -8,8 +8,12 @@ import mequie.app.domain.Group;
 import mequie.app.domain.Message;
 import mequie.app.domain.User;
 import mequie.utils.WriteInDisk;
+import mequie.utils.ReadFromDisk;
 
 public class SaveToDiskHandler {
+	
+	private static final String GROUPFILE = "Data/group.txt";
+	private static final String PASSWDFILE = "Data/passwd.txt";
 	
 	private SaveToDiskHandler() {}
 	
@@ -22,7 +26,7 @@ public class SaveToDiskHandler {
 			stringsToWrite.add(m.getInfo());
 			
 			// save
-			WriteInDisk writer = new WriteInDisk("Data/" + g.getGoupID() + "_msgs.txt");			
+			WriteInDisk writer = new WriteInDisk("Data/" + g.getGoupID() + "/messages.txt");			
 			writer.saveListOfStringsSeparatedBy(stringsToWrite, ":");
 			return true;
 		} catch (IOException e) {
@@ -32,7 +36,7 @@ public class SaveToDiskHandler {
 	
 	public static boolean savePhotoMessageInDisk(Message m, Group g) {
 		try {
-			WriteInDisk writer = new WriteInDisk("Data/" + g.getGoupID() + "_" + m.getMsgID() + ".txt");		
+			WriteInDisk writer = new WriteInDisk("Data/" + g.getGoupID() + "/" + m.getMsgID() + ".txt");		
 			writer.saveSimpleString(m.getInfo()); //esta a escrever a path e nao o conteudo em si REVER
 			return true;
 		} catch (IOException e) {
@@ -42,7 +46,7 @@ public class SaveToDiskHandler {
 	
 	public static boolean saveUserInDisk(User u) {
 		try {
-			WriteInDisk write = new WriteInDisk("Data/passwd.txt");
+			WriteInDisk write = new WriteInDisk(PASSWDFILE);
 			write.saveTwoStringsSeparatedBy(u.getUserID(), u.getPassword(), ":");
 			return true;
 		} catch (IOException e) {
@@ -52,7 +56,7 @@ public class SaveToDiskHandler {
 	
 	public static boolean saveGroupInDisk(Group g) {
 		try {
-			WriteInDisk write = new WriteInDisk("Data/group.txt");
+			WriteInDisk write = new WriteInDisk(GROUPFILE);
 			write.saveTwoStringsSeparatedBy(g.getGoupID(), g.getOwner().getUserID(), ":");
 			return true;
 		} catch (IOException e) {
@@ -62,7 +66,7 @@ public class SaveToDiskHandler {
 	
 	public static boolean saveUserToGroupInDisk(User u, Group g) {
 		try {
-			WriteInDisk write = new WriteInDisk("Data/group.txt");
+			WriteInDisk write = new WriteInDisk(GROUPFILE);
 			// ir buscar toda a info e reescrever toda mas com a correcao
 			write.saveStringSeparatedBy(u.getUserID(), ":");
 			return true;
@@ -75,7 +79,7 @@ public class SaveToDiskHandler {
 		try {
     		// ler as linhas e ver o grupo que foi alterado e remover do disco
     		// depois escrever a alteracao ao grupo no disco
-			WriteInDisk write = new WriteInDisk("Data/group");
+			WriteInDisk write = new WriteInDisk(GROUPFILE);
 			return true;
 		} catch (IOException e) {
 			return false;
