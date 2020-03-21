e54ea585f4a2d3cb4d6c40de6919a52c17796dbc
Marcelo Mouta <fc51110@alunos.fc.ul.pt>
2020-03-21 00:54:53 +0000

    updated path names in Configuration

--- server/src/mequie/utils/Configuration.java
+++ server/src/mequie/utils/Configuration.java
@@ -8,14 +8,14 @@ public class Configuration {
 	private static final String GROUPFILENAME = "group.txt";
 	private static final String PASSWDFILE = "passwd.txt";
 	private static final String MSG_USERS_FILENAME = "messages_users.txt";
-	private static final String MSG_CONTENTS_FILENAME = "messages_contents.txt";
+	private static final String TEXT_MSGS_FILENAME = "text_messages.txt";
 	
 	public static String getMessageUsersPathName(String groupID) {
 		return DATAPATH + groupID + MSG_USERS_FILENAME;
 	}
 	
 	public static String getMessageContentsPathName(String groupID) {
-		return DATAPATH + groupID + MSG_CONTENTS_FILENAME;
+		return DATAPATH + groupID + TEXT_MSGS_FILENAME;
 	}
 	
 	public static String getGroupPathName() {
