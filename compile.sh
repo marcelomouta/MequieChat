69cc0659e1b5ed2cd95f2727561c63c6472bc2bf
Bruno Freitas <fc51468@alunos.fc.ul.pt>
2020-03-22 19:45:26 +0000

    added compile.sh to compile the project

---
+++ compile.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -d "server/bin" ] ; then
+    rm -Rf server/bin/*
+fi
+
+if [ -d "client/bin" ] ; then
+    rm -Rf client/bin/*
+fi
+
+# compile server
+javac -d server/bin -cp server/src server/src/mequie/main/MequieServer.java
+
+#compile client
+javac -d client/bin -cp client/src:server/src client/src/mequie/main/Mequie.java
