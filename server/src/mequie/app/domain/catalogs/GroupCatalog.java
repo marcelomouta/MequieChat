fe99560b481391a1d22ff389ca876076e9858dfa
Bruno Freitas <fc51468@alunos.fc.ul.pt>
2020-03-22 00:22:00 +0000

    javadoc of catalogs

--- server/src/mequie/app/domain/catalogs/GroupCatalog.java
+++ server/src/mequie/app/domain/catalogs/GroupCatalog.java
@@ -10,16 +10,22 @@ import mequie.app.domain.Group;
  * 
  * @author 51021 Pedro Marques,51110 Marcelo Mouta,51468 Bruno Freitas
  * 
- * Esta classe representa o Catalogo de Grupos que contem todos 
- * os grupos do sistema
+ * This class represents a Group Catalog that contains all groups
+ * of the system
  */
 
  public class GroupCatalog {
     private static GroupCatalog INSTANCE;
+    
+    // all groups of the system
     private Set<Group> groups = new HashSet<>();
 
     private GroupCatalog(){}
 
+    /**
+     * 
+     * @retrun the instance of this class
+     */
     public static GroupCatalog getInstance(){
 		if(INSTANCE == null){
 			INSTANCE = new GroupCatalog();
@@ -27,10 +33,20 @@ import mequie.app.domain.Group;
 		return INSTANCE;
     }
     
+    /**
+     * 
+     * @param group the group to be added
+     * @return true if group was successfully added to the catalog
+     */
     public boolean addGroup(Group grupo) {
         return groups.add(grupo);
     }
 
+    /**
+     * 
+     * @param groupID the groupID of the possible group
+     * @return the group if a group with groupID id exists or null
+     */
     public Group getGroupByID(String groupID) {
         for (Group g : groups) {
             if (g.getGoupID().equals(groupID)) {
@@ -41,6 +57,10 @@ import mequie.app.domain.Group;
         return null;
     }
     
+    /**
+     * 
+     * @return a list of all groups
+     */
     public List<Group> getAllGroups() {
     	return new ArrayList<>(groups);
     }
--- server/src/mequie/app/domain/catalogs/UserCatalog.java
+++ server/src/mequie/app/domain/catalogs/UserCatalog.java
@@ -10,15 +10,21 @@ import mequie.app.domain.User;
  * 
  * @author 51021 Pedro Marques,51110 Marcelo Mouta,51468 Bruno Freitas
  * 
- * Esta classe representa o Catalogo de Users que contem todos 
- * os users do sistema
+ * This class represents a Group Catalog that contains all users
+ * of the system
  */
 public class UserCatalog {
     private static UserCatalog INSTANCE;
+    
+    // all users of thhe system
     private Set<User> users = new HashSet<>();
 
     private UserCatalog(){}
 
+    /**
+     * 
+     * @retrun the instance of this class
+     */
     public static UserCatalog getInstance(){
 		if(INSTANCE == null){
 			INSTANCE = new UserCatalog();
@@ -26,14 +32,30 @@ public class UserCatalog {
 		return INSTANCE;
     }
     
-    public User createUser(String username, String password) {
-		return new User(username, password);
+    /**
+     * 
+     * @param username the username of the user
+     * @param passwd the password of the user
+     * @return new User with username username and password passwd
+     */
+    public User createUser(String username, String passwd) {
+		return new User(username, passwd);
     }
     
+    /**
+     * 
+     * @param user the user to be added
+     * @return true if user was successfully added to the catalog
+     */
     public boolean addUser(User u) {
         return users.add(u);
     }
 
+    /**
+     * 
+     * @param userID the userID of the possible user
+     * @return the user if a user with userID id exists or null
+     */
     public User getUserById(String userID) {
         for (User u : users) {
             if (u.getUserID().equals(userID)) {
@@ -44,6 +66,10 @@ public class UserCatalog {
         return null;
     }
     
+    /**
+     * 
+     * @return a list of all users
+     */
     public List<User> getAllUsers() {
     	return new ArrayList<>(users);
     }
